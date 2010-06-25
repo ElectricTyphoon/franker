dbff5d647b553fe0dbf1863700afb7e769f3bc2d
ysoldak <ysoldak@715b8d89-62b9-a2e4-5eea-26f9e522f3c1>
2010-06-25 11:43:35 +0000

    initial version with global frankate disabled (frankate selection works only)

---
+++ trunk/Franker.safariextension/frankate.js
@@ -0,0 +1,96 @@
+function getSelectedText() {
+	var txt = '';
+	if (window.getSelection) {
+		txt = window.getSelection();
+	} else if (document.getSelection) {
+		txt = document.getSelection();
+	} else if (document.selection) {
+		txt = document.selection.createRange().text;
+	}
+	return txt;
+}
+
+function frankateSelection() {
+	var text = getSelectedText();
+	if (text + "" != "") {
+//		console.log("selected: " + text);
+		safari.self.tab.dispatchMessage("frankateSelectionRequest", text + "");
+	}
+}
+
+function handleMessage(msgEvent) {
+	if (msgEvent.name == "frankateSelection") {
+		frankateSelection();
+	} else if (msgEvent.name == "frankateSelectionResponse") {
+//		console.log("translated:" + msgEvent.message);
+		var text = getSelectedText();
+		if (text + "" != "") {
+			var translationText = document.createTextNode("(" + msgEvent.message + ")");
+			var translationNode = document.createElement("span");
+			translationNode.setAttribute("class", "google-dst-text");
+			translationNode.appendChild(translationText);
+			if (text.baseNode.nextSibling) {
+				text.baseNode.parentNode.insertBefore(translationNode, text.baseNode.nextSibling);
+			} else {
+				text.baseNode.parentNode.appendChild(translationNode);
+			}
+		}
+	}
+}
+safari.self.addEventListener("message", handleMessage, false);
+
+shortcut.add("Ctrl+Shift+F",function() {
+	frankateSelection();
+},{
+		'type':'keydown',
+		'propagate':true,
+		'target':document
+});
+
+
+// the real franker
+function frankate() {
+	var spans = document.getElementsByTagName('span');
+	var i;
+	for (i = 0; i < spans.length; i++) {
+		if (spans[i].getAttribute('onmouseover')) {
+			var dstSpan = spans[i];
+			var srcSpan = spans[i+1];
+			dstSpan.removeAttribute('style');
+			dstSpan.setAttribute('class', 'google-dst-text');
+			
+			dstSpan.removeAttribute('onmouseover');
+			dstSpan.setAttribute('onclick', '_tipon(this)');
+
+			//// uncomment if want tooltip not hide on mouseout
+			//spans[i].removeAttribute('onmouseout');
+
+			//// moving the source text's span out of the current span (not required now)
+			dstSpan.parentNode.insertBefore(srcSpan, dstSpan);
+			//spans[i].removeChild(spans[i+1]);
+			
+			// wrapping current sentence in para (separate sentences)
+			//var separator = document.createElement('p');
+			//spans[i].parentNode.insertBefore(separator,spans[i]);
+			//separator.appendChild(spans[i]);
+			
+			//// alternative separation strategies
+			//var separator0 = document.createElement('br');
+			//spans[i].parentNode.insertBefore(separator0,spans[i]);
+			//var separator1 = document.createElement('br');
+			//spans[i].parentNode.insertBefore(separator1,spans[i]);
+			
+			//// uncomment you if want original and translated text to be separated by linefeed
+			//var separator2 = document.createElement('br');
+			//spans[i].insertBefore(separator2,spans[i+1].nextSibling);
+			
+			var left = document.createTextNode('(');
+			var right = document.createTextNode(')');
+			dstSpan.insertBefore(left, dstSpan.firstChild);
+			dstSpan.appendChild(right);
+			
+		}
+	}
+}
+
+//frankate();
