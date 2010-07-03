99f292e4e90101fec705ef84a12fb8128aff5e74
ysoldak <ysoldak@715b8d89-62b9-a2e4-5eea-26f9e522f3c1>
2010-07-03 00:59:35 +0000

    0.9.0

--- Franker.safariextension/frankate.js
+++ Franker.safariextension/frankate.js
@@ -6,38 +6,43 @@ function handleMessage(msgEvent) {
 	if (msgEvent.name == "frankateSelection") {
 		frankateSelection();
 	} else if (msgEvent.name == "frankateSelectionResponse") {
-		//injectTranslationForSelection(msgEvent);
 		injectTranslationForSentence(msgEvent);
 	} else if (msgEvent.name == "frankatePage") {
 		frankatePage();
-	// } else if (msgEvent.name == "transformGoogleTranslationBlocks") {
-	// 	transformGoogleTranslationBlocks();
 	} else if (msgEvent.name == "shortcutFrankateSelectionValue") {
-		setFrankateSelectionShortcut(msgEvent);
+		setShortcut(msgEvent, frankateSelection);
 	} else if (msgEvent.name == "shortcutFrankatePageValue") {
-		setFrankatePageShortcut(msgEvent);
+		setShortcut(msgEvent, frankatePage);
+	} else if (msgEvent.name == "shortcutFrankateClearValue") {
+		setShortcut(msgEvent, frankateClear);
 	} else if (msgEvent.name == "styleDestinationValue") {
 		frankerUserStyle = msgEvent.message;
 	} else if (msgEvent.name == "statePageEnabledValue") {
-		if (msgEvent.message == true && document.location.href.indexOf("translate.googleusercontent.com", 0) >= 0) {
-			transformGoogleTranslationBlocks();
+		if (msgEvent.message == true) {
+			if (document.location.href.indexOf("translate.googleusercontent.com", 0) >= 0) {
+				transformGoogleTranslationBlocks();
+			// } else if (document.location.href.indexOf("translate.google.com") >= 0) {
+			// 	for (var i = 0; i < window.frames.length; i++) {
+			// 		var src = window.frames[i].document.location;
+			// 		if (src.indexOf('translate_p') >= 0 && src.indexOf('usg=') >= 0) {
+			// 			document.location = "http://translate.googleusercontent.com" + unescape(src);
+			// 		}
+			// 	}
+			}
 		}
 	}
 }
 
+
 // ==== Shortcuts ====
 
-function setFrankateSelectionShortcut(msgEvent) {
-	//console.log(document.location.href);
-	//console.log("Setting shortcut: " + msgEvent.message);
+function setShortcut(msgEvent, func) {
 	var values = msgEvent.message.split(":");
 	if (values.length > 1) {
 		shortcut.remove(values[1]);
 	}
 	shortcut.remove(values[0]); // must remove first to ensure we do not duplicate the shortcut
-	shortcut.add(values[0], function() {
-		frankateSelection();
-	},{
+	shortcut.add(values[0], func, {
 			'type':'keydown',
 			'propagate':false,
 			'disable_in_input':true,
@@ -45,23 +50,6 @@ function setFrankateSelectionShortcut(msgEvent) {
 	});
 }
 
-function setFrankatePageShortcut(msgEvent) {
-	//console.log(document.location.href);
-	//console.log("Setting shortcut: " + msgEvent.message);
-	var values = msgEvent.message.split(":");
-	if (values.length > 1) {
-		shortcut.remove(values[1]);
-	}
-	shortcut.remove(values[0]); // must remove first to ensure we do not duplicate the shortcut
-	shortcut.add(values[0], function() {
-		frankatePage();
-	},{
-			'type':'keydown',
-			'propagate':false,
-			'disable_in_input':true,
-			'target':document
-	});
-}
 
 // ==== Whole Page ====
 
@@ -77,14 +65,13 @@ function transformGoogleTranslationBlocks() {
 			var dstSpan = spans[i];
 			var srcSpan = spans[i+1];
 			dstSpan.removeAttribute('style');
-			dstSpan.setAttribute('class', 'google-dst-text');
+			dstSpan.setAttribute('class', 'franker-dst-text');
 			dstSpan.setAttribute("style", frankerUserStyle);
 			
 			dstSpan.removeAttribute('onmouseover');
-			dstSpan.setAttribute('onclick', '_tipon(this)');
+			spans[i].removeAttribute('onmouseout');
+			//dstSpan.setAttribute('onclick', '_tipon(this)');
 
-			//// uncomment if want tooltip not hide on mouseout
-			//spans[i].removeAttribute('onmouseout');
 
 			//// moving the source text's span out of the current span (not required now)
 			dstSpan.parentNode.insertBefore(srcSpan, dstSpan);
@@ -112,9 +99,9 @@ function transformGoogleTranslationBlocks() {
 			dstSpan.appendChild(right);
 			
 		}
-		if (spans[i].className == "google-src-text") {
-			spans[i].style.display = "inline !important";
-		}
+		// if (spans[i].className == "google-src-text") {
+		// 	spans[i].style.display = "inline !important";
+		// }
 	}
 }
 
@@ -137,8 +124,6 @@ function getSelectedStringTrimmed() {
 }
 
 var frankerSelection;
-//var frankerDot;
-//var frankerEndMarker;
 var frankerInfinityGuard = 50;
 var frankerUserSelection;
 var frankerSelectionHasEndOfSentence = false;
@@ -148,39 +133,12 @@ function frankateSelection() {
 	if (selection != "") {
 		frankerSelection = getSelectedText();
 		frankerUserSelection = frankerSelection.getRangeAt(0).cloneRange();
-		//frankerDot = document.createTextNode('. ');
-		//frankerEndMarker = document.createTextNode('Hey, Frank!');
-		
-		// if (frankerSelection.focusOffset == 0) {
-		// 	if (!selection.match(/[.!?]+$/g)) {
-		// 		frankerSelection.anchorNode.parentNode.appendChild(frankerDot);
-		// 	}
-		// 	frankerSelection.anchorNode.parentNode.appendChild(frankerEndMarker);
-				// } else if (frankerSelection.focusNode.nextSibling) {
-				// 	frankerSelection.focusNode.parentNode.insertBefore(frankerDot, frankerSelection.focusNode.nextSibling);
-				// 	frankerSelection.focusNode.parentNode.insertBefore(frankerEndMarker, frankerSelection.focusNode.nextSibling);
-				// } else {
-				// 	frankerSelection.focusNode.parentNode.appendChild(frankerDot);
-				// 	frankerSelection.focusNode.parentNode.appendChild(frankerEndMarker);
-		// } else {
-		// 	var tmpRange = frankerSelection.getRangeAt(0).cloneRange();
-		// 	tmpRange.collapse(false);
-		// 	tmpRange.insertNode(frankerEndMarker);
-		// 	if (!selection.match(/[.!?]+$/g)) {
-		// 		tmpRange.insertNode(frankerDot);
-		// 	}
-		// 	tmpRange.detach();
-		// }
 		frankerSelectionHasEndOfSentence = selection.match(/[.!?]+/g);
 		if (frankerSelectionHasEndOfSentence) {
 			frankerSelection.collapseToStart();
 		}
 		
 		translateNextSentence();
-		
-		// for (var i = 0; i < srcArr.length; i++) {
-		// 	safari.self.tab.dispatchMessage("frankateSelectionRequest", text + "");
-		// }
 	}
 }
 
@@ -192,18 +150,12 @@ function translateNextSentence() {
 	}
 
 	var boundaryCheck = frankerUserSelection.compareBoundaryPoints(Range.START_TO_END, frankerSelection.getRangeAt(0));
-	if (frankerInfinityGuard < 0 || boundaryCheck <= 0) { //frankerSelection.containsNode(frankerEndMarker) || ) {
+	if (frankerInfinityGuard < 0 || boundaryCheck <= 0) {
 		if (frankerInfinityGuard < 0) {
 			console.log("Franker Safari Extension: Too many sentences to frankate, maximum (50) reached and the process was stopped.");
 		}
 		frankerSelection.collapse();
 		delete(frankerSelection);
-		// if (frankerDot.parentNode) {
-		// 	frankerDot.parentNode.removeChild(frankerDot);
-		// }
-		// delete(frankerDot);
-		// frankerEndMarker.parentNode.removeChild(frankerEndMarker);
-		// delete(frankerEndMarker);
 		frankerInfinityGuard = 50;
 		return;
 	}
@@ -220,114 +172,36 @@ function injectTranslationForSentence(msgEvent) {
 	if (!frankerSelection) {
 		return;
 	}
-	var translationText = document.createTextNode(" (" + msgEvent.message.replace(/&quot;/g, "\"").replace(/&#39;/g, "'").replace(/&amp;/g, "&") + ") ");
-	var translationNode = document.createElement("span");
-	translationNode.setAttribute("class", "google-dst-text");
+	var cleanText = msgEvent.message.replace(/&quot;/g, "\"").replace(/&#39;/g, "'").replace(/&amp;/g, "&");
+	var translationText = document.createTextNode(" (" + cleanText + ") ");
+	var translationNode = document.createElement("franker");
+	translationNode.setAttribute("class", "franker-dst-text");
 	translationNode.setAttribute("style", frankerUserStyle);
 	translationNode.appendChild(translationText);
 	
-	if (frankerSelection.focusOffset == 0) {
-		frankerSelection.anchorNode.parentNode.appendChild(translationNode);
-		frankerSelection.collapseToEnd();
-	} else {
-		frankerSelection.collapseToEnd();
-		frankerSelection.getRangeAt(0).insertNode(translationNode);
-		frankerSelection.modify("move","forward","sentence");
-	}
+	// need this small hack to ensure we inject translation directly after the original text,
+	// since 'modify("extend","forward","sentence")' selects also a whitechar after the sentence (usually space);
+	// also focus node becomes next paragraph if the sentence was the last one in current paragraph
+	frankerSelection.modify("extend","backward","character");
+	
+	frankerSelection.collapseToEnd();
+	frankerSelection.getRangeAt(0).insertNode(translationNode);
+	frankerSelection.modify("move","forward","sentence");
 	
 	translateNextSentence();
 }
 
-function injectTranslationForSelection(msgEvent) {
-	//console.log(document.location.href);
-	//console.log("translated:" + msgEvent.message);
-	var text = getSelectedText();
-	if (text + "" != "") {
-		var src = (text + "").replace(/^\s*/, "").replace(/\s*$/, "");
-		var srcArr = src.split(/[.!?]+/);
-		var dst = msgEvent.message;
-		var dstArr = dst.split(/[.!?]+/);
-		
-		//var range = text.range;
-		text.collapseToStart();
-		text.modify("extend","forward","sentence");
-		var nodesToRemove = {};
-		for (var i = 0; i < srcArr.length; i++) {
-			if (srcArr[i] == "") {
-				continue;
-			}
-			var tmpSel = getSelectedText();
-			//console.log(i + ":" + tmpSel + "/" + text);
-			//console.log(tmpSel.focusOffset + ":" + tmpSel.focusNode.nodeValue);
-			var selected = (tmpSel + "").replace(/^\s*/, "").replace(/\s*$/, "");
-			//alert(text.anchorOffset + "/" + text.focusOffset + ":" + tmpSel + ":" + text.anchorNode.nodeValue + ":" + text.focusNode.nodeValue);
-			var translationText = document.createTextNode(" (" + dstArr[i].replace(/^\s*/, "").replace(/\s*$/, "") + selected.substring(selected.length - 1) + ") ");
-			var translationNode = document.createElement("span");
-			translationNode.setAttribute("class", "google-dst-text");
-			translationNode.appendChild(translationText);
-			
-			if (text.focusOffset == 0) {
-				//tmpSel.anchorNode.nodeValue += " (" + dstArr[i] + ") ";
-			//	text.getRangeAt(0).setEndBefore(text.baseNode.parentNode.nextSibling);
-			//}
-			//console.log(tmpSel.focusNode + "/" + tmpSel.focusOffset);
-				text.anchorNode.parentNode.appendChild(translationNode);
-				//var baseTextBefore = document.createTextNode(text.baseNode.nodeValue.substring(0, text.focusOffset));
-				//var focusTextAfter = document.createTextNode(text.focusNode.nodeValue.substring(text.focusOffset) + 1);
-				//focusParent.insertBefore(focusTextAfter, text.focusNode);
-				//focusParent.insertBefore(translationNode, focusTextAfter);
-				//focusParent.insertBefore(focusTextBefore, translationNode);
-				text.collapseToEnd();
-			} else {
-				// tmpSel.focusNode.nodeValue = tmpSel.focusNode.nodeValue.substring(0, tmpSel.focusOffset) + " (" + dstArr[i] + tmpSel.focusNode.nodeValue.substring(tmpSel.focusOffset - 2, tmpSel.focusOffset - 1) + ") " + tmpSel.focusNode.nodeValue.substring(tmpSel.focusOffset) + 1;
-			// }
-				// var focusParent = text.focusNode.parentNode;
-				// var focusTextBefore = document.createTextNode(text.focusNode.nodeValue.substring(0, text.focusOffset));
-				// var focusTextAfter = document.createTextNode(text.focusNode.nodeValue.substring(text.focusOffset) + 1);
-				// focusParent.insertBefore(focusTextAfter, text.focusNode);
-				// focusParent.insertBefore(translationNode, focusTextAfter);
-				// focusParent.insertBefore(focusTextBefore, translationNode);
-				// //focusParent.removeChild(text.focusNode);
-				// nodesToRemove[nodesToRemove.length] = text.focusNode;
-				text.collapseToEnd();
-				text.getRangeAt(0).insertNode(translationNode);
-				text.modify("move","forward","sentence");
-			}
-			
-			// text.collapseToEnd();
-			//text.modify("move","forward","sentence");
-			text.modify("extend","forward","sentence");
-			//focusParent.removeChild(toRemove);
-		}
-		// for (var i = 0; i < nodesToRemove.length; i++) {
-		// 	nodesToRemove[i].parentNode.removeChild(nodesToRemove[i]);
-		// }
-		// text.focusNode.data = text.focusNode.data.substring(0, text.focusOffset) + "<span class=\"google-dst-text\">"+msgEvent.message+"</span>" + text.focusNode.data.substring(text.focusOffset + 1);
-		
-		// var translationText = document.createTextNode(" (" + dst + ") ");
-		// var translationNode = document.createElement("span");
-		// translationNode.setAttribute("class", "google-dst-text");
-		// translationNode.appendChild(translationText);
-		// 
-		// var focusParent = text.focusNode.parentNode;
-		// var focusTextBefore = document.createTextNode(text.focusNode.data.substring(0, text.focusOffset));
-		// var focusTextAfter = document.createTextNode(text.focusNode.data.substring(text.focusOffset) + 1);
-		// focusParent.insertBefore(focusTextAfter, text.focusNode);
-		// focusParent.insertBefore(translationNode, focusTextAfter);
-		// focusParent.insertBefore(focusTextBefore, translationNode);
-		// focusParent.removeChild(text.focusNode);
-		
-		// if (text.focusNode.nextSibling) {
-		// 	text.focusNode.parentNode.insertBefore(translationNode, text.focusNode.nextSibling);
-		// } else {
-		// 	text.focusNode.parentNode.appendChild(translationNode);
-		// }
-		
-		// if (text.baseNode.nextSibling) {
-		// 	text.baseNode.parentNode.insertBefore(translationNode, text.baseNode.nextSibling);
-		// } else {
-		// 	text.baseNode.parentNode.appendChild(translationNode);
-		// }
+
+// ==== Initial ====
+
+function frankateClear() {
+	// if (document.location.href.indexOf('translate.googleusercontent.com') >= 0) {
+	// 	var urlEscaped = document.location.href.replace(/.*u=(.*)&rurl=.*/g, "$1");
+	// 	document.location = unescape(urlEscaped);
+	// }
+	var nodes = document.getElementsByClassName('franker-dst-text');
+	while (nodes.length > 0) {
+		nodes[0].parentNode.removeChild(nodes[0]);
 	}
 }
 
@@ -338,5 +212,6 @@ safari.self.addEventListener("message", handleMessage, false);
 
 safari.self.tab.dispatchMessage("shortcutFrankateSelectionRequest", "");
 safari.self.tab.dispatchMessage("shortcutFrankatePageRequest", "");
+safari.self.tab.dispatchMessage("shortcutFrankateClearRequest", "");
 safari.self.tab.dispatchMessage("styleDestinationRequest", "");
 safari.self.tab.dispatchMessage("statePageEnabledRequest", "");
