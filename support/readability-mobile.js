4f8ee63869ced19cc9a61c4052c8cfe7b939d713
ysoldak <ysoldak@715b8d89-62b9-a2e4-5eea-26f9e522f3c1>
2011-03-13 16:31:52 +0000

    hide readability tip about navigation

--- support/readability-mobile.js
+++ support/readability-mobile.js
@@ -2,8 +2,13 @@ function frankerReadabilityUnbindClick() {
 	$('article.article').find('section.article-content, #menu-tip').unbind('click');
 }
 
+function frankerReadabilityHideTip() {
+	readability.mobile.single.hideMenuTip(true)
+}
+
 function frankerReadabilityToggleStyles() {
 	readability.mobile.single.toggleStyles(true);
 }
 
 frankerReadabilityUnbindClick();
+frankerReadabilityHideTip();
