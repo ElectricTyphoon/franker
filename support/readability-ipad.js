956b733a7a01cda943f778a6803ed1abe227ccb6
ysoldak <ysoldak@715b8d89-62b9-a2e4-5eea-26f9e522f3c1>
2011-08-24 20:18:40 +0000

    enabling back: a) text select and b) scroll after Readability changed their mobile page presentation

--- support/readability-ipad.js
+++ support/readability-ipad.js
@@ -1,8 +1,11 @@
 function frankerReadabilityUnbindClick() {
-	$('article.article').unbind('click');
-	$('article.article').unbind('touchmove');
-	$('article.article').find('section.article-content, #menu-tip').unbind('click');
-	$('article.article').find('section.article-content, #menu-tip').unbind('touchmove');
+	$('#rdb-application')[0].appendChild($('#rdb-article')[0]);
+	$('#rdb-application')[0].appendChild($('#rdb-footer')[0]);
+	$('#rdb-application')[0].removeChild($('#rdb-scrollwrap-outer')[0]);
+	//$('article.article').unbind('click');
+	//$('article.article').unbind('touchmove');
+	//$('article.article').find('section.article-content, #menu-tip').unbind('click');
+	//$('article.article').find('section.article-content, #menu-tip').unbind('touchmove');
 }
 
 function frankerReadabilityHideTip() {
@@ -10,7 +13,8 @@ function frankerReadabilityHideTip() {
 }
 
 function frankerReadabilityToggleStyles() {
-	readability.mobile.single.toggleStyles(true);
+	$("#rdb-actions a[href='#settings']").trigger('click');	
+	//readability.mobile.single.toggleStyles(true);
 	//readability.mobile.single.toggleNav(true);
 }
 
